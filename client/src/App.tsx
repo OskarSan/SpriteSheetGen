a5165193c8033d37003b26c36dcd45283a60150e
Lasstuu <lassituo@gmail.com>
2025-05-17 16:37:53 +0300

    uityöst

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { Padding } from '@mui/icons-material';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 function App() {
 
@@ -7,6 +23,8 @@ function App() {
   const [response, setResponse] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [uploadedImage, setUploadedImage] = useState<File | null>(null);
+  const [previewImage, setPreviewImage] = useState<string | null>(null); // State for previewing the uploaded image
+
 
   const handleSend = async () => {
     const formData = new FormData();
@@ -29,16 +47,39 @@ function App() {
     const file = event.target.files?.[0];
     if (file) {
       setUploadedImage(file);
+      setPreviewImage(URL.createObjectURL(file));
     }
   };
 
   return (
     <>
       <h1>AI Spritesheet Generator</h1>
-      
       <br/>
-      <input type="file" accept="image/png" onChange={handleImageUpload} />
-      {uploadedImage && <p>Uploaded Image: {uploadedImage.name}</p>}
+      
+      <Button
+        component="label"
+        role={undefined}
+        variant="contained"
+        tabIndex={-1}
+        
+        startIcon={<CloudUploadIcon />}
+      >
+        Upload files
+        <VisuallyHiddenInput
+          type="file"
+          onChange={handleImageUpload}
+          multiple
+        />
+      </Button>
+      <br/>
+      <br/>
+      {previewImage && (
+        <div>
+          <h3>Uploaded Image Preview:</h3>
+          <img src={previewImage} alt="Uploaded Preview" style={{ maxWidth: '300px', maxHeight: '300px' }} />
+        </div>
+      )}
+      
       <button onClick={handleSend}>Send</button>
       <div>{response}</div>
     </>
